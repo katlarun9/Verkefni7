3d8ed07d30a01f189f9c91968976d26ee8b78313
kri9 <kri9@hi.is>
2018-10-30 15:30:27 +0000

    verk7

--- script.js
+++ script.js
@@ -13,7 +13,33 @@ const GAMES_TO_PLAY = 10;
  * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
  */
 function start() {
-  villa;
+  alert('Markmiðið er að svara eins mörgum af 10 dæmum rétt eins hratt og mögulegt er');
+  do {
+    let fjoldi = 0;
+    let timi = 0;
+    let medal;
+    let spila;
+    let b = {};
+    for(let i=1; i<=GAMES_TO_PLAY; i++){
+      spila = play(); 
+      console.log(spila);
+      if(spila==0) {
+        break;
+      }
+      if(spila.teljari == 1) {
+        fjoldi++;
+      }  
+      
+      timi = timi + spila.time;
+
+
+    }
+    medal = fjoldi/timi;
+    if(spila !=0){
+      alert('Þú svaraðir ' + fjoldi + ' af 10 dæmum rétt á ' + timi.toFixed(2) + " sekúndum \nMeðalrétt svör á sekúndu eru " + medal.toFixed(2));  
+    }
+    }while (confirm('Viltu spila aftur?'))
+
 }
 
 /**
@@ -28,6 +54,23 @@ function start() {
  *
  */
 function play() {
+  let startTime = new Date();
+  let count = 0;
+  let correct = ask();
+  if (correct == true) {
+    count++;
+  }
+  if(correct == 0) {
+    return 0;
+  }
+  let endTime = new Date();
+  let total = (endTime - startTime)/1000;
+
+  let a = {
+    time: total,
+    teljari: count,
+  }
+  return a;
 }
 
 /**
@@ -45,6 +88,65 @@ function play() {
  * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
  */
 function ask() {
+  const operators = ['+', '-', '/', '*'];
+  const questions = operators[randomNumber(0, operators.length-1)];
+  let number1;
+  let number2;
+  let b;
+
+  switch(questions){
+    case '+':
+    number1 = randomNumber(1, 100);
+    number2 = randomNumber(1, 100);
+    b = prompt('Hvað er ' + number1 + '+' + number2 + "?");
+    if(number1+number2 == b) {
+      return true;
+    }
+    if (b === null) {
+      alert('Hætt í leik');
+      return 0;
+    }
+    break;
+
+    case '-':
+    number1 = randomNumber(1, 100);
+    number2 = randomNumber(1, 100);
+    b = prompt('Hvað er ' + number1 + " - " + number2 + "?");
+    if(number1-number2 == b) {
+      return true;
+    }
+    if (b === null) {
+      alert('Hætt í leik');
+      return 0;
+    }
+    break;
+
+    case '*':
+    number1 = randomNumber(1, 10);
+    number2 = randomNumber(1, 10);
+    b = prompt('Hvað er ' + number1 + " * " + number2 + "?");
+    if(number1*number2 == b) {
+      return true;
+    }
+    if (b === null) {
+      alert('Hætt í leik');
+      return 0;
+    }
+    break;
+
+    case '/':
+    number1 = randomNumber(2, 10);
+    number2 = number1 * randomNumber(2, 10);
+    b = prompt('Hvað er ' + number2 + " / " + number1 + "?");
+    if(number2/number1 == b) {
+      return true;
+    }
+    if (b === null) {
+      alert('Hætt í leik');
+      return 0;
+    }
+    break;
+  }
 }
 
 /**
